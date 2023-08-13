0f27c2aeb0cad33aeeb31c88524572664b1e975a
Michael Truong <truong.minh.michael@gmail.com>
2023-08-12 17:52:00 -0700

    Added token to createUser, and added test

---
+++ graphql-api/tests/middleware_test.go
@@ -0,0 +1,36 @@
+// resolver_test.go
+package test
+
+import (
+	"context"
+	"testing"
+
+	// Import your models package
+	"github.com/michael-m-truong/fitness-tracker/graph"
+	"github.com/michael-m-truong/fitness-tracker/graph/model"
+	mock "github.com/michael-m-truong/fitness-tracker/mocks"
+	"github.com/stretchr/testify/assert" // Import the testify assertion library
+)
+
+func TestCreateUser(t *testing.T) {
+	// Mocked data and context
+	mockContext := context.Background() // Create a mocked context
+	mockInput := model.NewUser{         // Mock resolver arguments
+		Username: "TestUser",
+		Password: "123",
+	}
+	expectedCharacter := "randomaccesstoken"
+
+	// Create a new instance of your resolver
+	resolver := &graph.Resolver{
+		AuthService: mock.MockAuthService{},
+	}
+
+	// Invoke the resolver function
+	result, err := resolver.Mutation().CreateUser(mockContext, mockInput)
+
+	// Assertions
+	assert.NoError(t, err)                     // No error should be returned
+	assert.NotNil(t, result)                   // Result should not be nil
+	assert.Equal(t, expectedCharacter, result) // Check if the result matches the expected character
+}
