5e3bc74382f15bafad6520bb0ef06bcba6db9b6c
Michael Truong <truong.minh.michael@gmail.com>
2023-08-19 13:29:40 -0700

    Add schema for workout

---
+++ sql/workout/workout_db.up.sql
@@ -0,0 +1,20 @@
+-- Create workout_exercise table
+CREATE TABLE "workout_exercise" (
+    "workout_id" BIGINT NOT NULL,
+    "exercise_id" BIGINT NOT NULL,
+    "duration" INTEGER NULL,
+    "sets" INTEGER NULL,
+    "reps" INTEGER NULL,
+    "weight" INTEGER NULL,
+    PRIMARY KEY ("workout_id", "exercise_id"),
+    CONSTRAINT "fk_workout_exercise_workout_id" FOREIGN KEY ("workout_id") REFERENCES "workout" ("id"),
+    CONSTRAINT "fk_workout_exercise_exercise_id" FOREIGN KEY ("exercise_id") REFERENCES "exercise" ("id")
+);
+
+-- Create workout table
+CREATE TABLE "workout" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "user_id" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL
+);
